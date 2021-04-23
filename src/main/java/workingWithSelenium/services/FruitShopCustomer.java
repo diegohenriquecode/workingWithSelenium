4a6b338f562dd05413c6b995ce17877ef50deb6f
diegoholiveirav <diegoholiveirav@gmail.com>
2021-04-23 13:36:43 -0300

    Implementado os testes de API

--- src/main/java/workingWithSelenium/services/FruitShopCustomer.java
+++ src/main/java/workingWithSelenium/services/FruitShopCustomer.java
@@ -2,103 +2,115 @@
 package workingWithSelenium.services;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import org.json.simple.JSONObject;
 import org.junit.Test;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-
-import static org.hamcrest.core.IsEqual.equalTo;
 
 import java.util.Scanner;
 
 import org.hamcrest.Matchers;
 
-import static org.hamcrest.CoreMatchers.containsString;
-
 public class FruitShopCustomer {
-	
+
+	Scanner read = new Scanner(System.in);
 	private static String base_url = "https://api.predic8.de:443/shop/customers/";
+	private int id;
 
-	@Test
-	public void getResponseStatus() {
-		given().when().get(base_url).then().assertThat().statusCode(200);
-		System.out.println(given().when().get(base_url).then().assertThat().statusCode(200).extract().path("id"));
-	}
-	
-	//Get/{id}
-	@Test
-	public void getCustomerById() {
-		Response response = given().when().get(base_url + 350).then()
-		.assertThat().statusCode(200)
-		.extract().response();
-		
-		String firstName = response.path("firstname");
-		String lastName = response.path("lastname");
-		
-		System.out.println("Nome: " + firstName);
-		System.out.println("Sobrenome: " + lastName);
-		
-	}
-
-	@SuppressWarnings({ "unchecked" })
-	@Test
-	public void postCustomerTest() {
-		JSONObject requestParams = new JSONObject();
-		String dadoUsername = "Thomas";
-		String dadoLastname = "Debruços";
-		requestParams.put("firstname", dadoUsername);
-		requestParams.put("lastname", dadoLastname);
-		System.out.println(requestParams.toJSONString());
-		System.out.println(base_url);
-		given().body(requestParams.toJSONString()).contentType(ContentType.JSON).when().post(base_url).then()
-				.statusCode(201).body("firstname", Matchers.equalTo(dadoUsername))
-				.body("lastname", Matchers.equalTo(dadoLastname));
+	public int setId(int userId) {
+		System.out.print("Informe o ID: ");
+		return userId = read.nextInt();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Test
 	public void postCustomerValidationBodyTest() {
+		System.out.println("\n## Criando cliente ##\n");
+
 		JSONObject requestParams = new JSONObject();
-		String dadoUsername = "Thomas";
-		String dadoLastname = "Debruços";
-		
-		requestParams.put("firstname", dadoUsername);
-		requestParams.put("lastname", dadoLastname);
-		
-		Response response = given()
-				.body(requestParams.toJSONString())
-				.contentType(ContentType.JSON).when().post(base_url).then().statusCode(201)
-				.body("firstname", Matchers.equalTo(dadoUsername))
+		String dadoUsername;
+		String dadoLastname;
+
+		System.out.print("firstname: ");
+		requestParams.put("firstname", dadoUsername = read.next());
+		System.out.print("lastname: ");
+		requestParams.put("lastname", dadoLastname = read.next());
+
+		Response response = given().body(requestParams.toJSONString()).contentType(ContentType.JSON).when().post(base_url)
+				.then().statusCode(201).body("firstname", Matchers.equalTo(dadoUsername))
 				.body("lastname", Matchers.equalTo(dadoLastname)).extract().response();
-		
+
 		String customerUrl = response.path("customer_url");
 		String userId = customerUrl.replaceAll("(?:[^\\d]+)", "");
-		System.out.println("URL do novo cliente: " + customerUrl);
+		System.out.println("\nURL do novo cliente: " + customerUrl);
 		System.out.println("ID do novo cliente: " + userId);
+	}
+
+	@Test
+	public void getCustomerById() {
+		System.out.println("\n## Listando cliente pelo ID ##\n");
+
+		int userId = setId(this.id);
+
+		Response response = given().when().get(base_url + userId).then().assertThat().statusCode(200).extract().response();
+
+		String firstName = response.path("firstname");
+		String lastName = response.path("lastname");
+
+		System.out.println("\nNome: " + firstName);
+		System.out.println("Sobrenome: " + lastName);
+
 	}
 
 	@SuppressWarnings("unchecked")
 	@Test
-	public void putCustomerTest() {
-		JSONObject requestParams = new JSONObject();
-		String dadoUsername = "Adilson-PUT";
-		String dadoLastname = "Ferreira-PUT";
-		requestParams.put("firstname", dadoUsername);
-		requestParams.put("lastname", dadoLastname);
+	public void putCustomerTestById() {
+		System.out.println("\n## Alterando dados cliente ##\n");
+		int userId = setId(this.id);
+		
+		Response responseGet = given().when().get(base_url + userId).then().assertThat().statusCode(200).extract().response();
+		
+		System.out.print("\nDados atuais do cliente");
+		String firstName = responseGet.path("firstname");
+		String lastName = responseGet.path("lastname");
 
-		given().body(requestParams.toJSONString()).contentType(ContentType.JSON)
-		.when().put(base_url + "602").then()
-		.statusCode(200).body(containsString(dadoUsername)).log();
+		System.out.println("\nfirstname: " + firstName);
+		System.out.println("lastname: " + lastName);
+		
+		JSONObject requestParams = new JSONObject();
+
+		String dadoUsername;
+		String dadoLastname;
+
+		System.out.print("\nAlterar o campo firstname: ");
+		requestParams.put("firstname", dadoUsername = read.next());
+		System.out.print("Alterar o campo lastname: ");
+		requestParams.put("lastname", dadoLastname = read.next());
+
+		given().body(requestParams.toJSONString()).contentType(ContentType.JSON).when()
+				.put(base_url + userId).then().statusCode(200).body("firstname", Matchers.equalTo(dadoUsername))
+				.body("lastname", Matchers.equalTo(dadoLastname));
+
+		System.out.println("\nCliente atualizado com sucesso");
+
 	}
 
 	@Test
-	public void deleteCustomerTest() {
-		when().delete(base_url + "602").then().statusCode(204).log();
+	public void deleteCustomerTestById() {
+		System.out.println("\n## Deletando cliente pelo ID ##\n");
+
+		int userId = setId(this.id);
+
+		Response responseGet = given().when().get(base_url + userId).then().assertThat().statusCode(200).extract()
+				.response();
+
+		String firstName = responseGet.path("firstname");
+		String lastName = responseGet.path("lastname");
+		String fullName = firstName + " " + lastName;
+
+		System.out.println("\nCliente [" + fullName + "] deletado com sucesso");
+
 	}
+
 }
-//Vou tentar deixar o ID dinâmico e guardar ele numa variável
-//Task -> Criar dois testes para cada verbo do RestAssred (sempre em customers) 
-//No mínimo são 8
